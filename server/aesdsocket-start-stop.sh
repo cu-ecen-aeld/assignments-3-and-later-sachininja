8884ed3fccf4c552e3580438553b69dade91b15f
sachininja <sama5040@colorado.edu>
2023-02-26 18:14:57 -0700

    fix: daemonize faiure fix in runqemu

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
+DAEMON_OPTS="-d"
 case "$1" in
     start)
         echo "Starting aesdsocket"
-        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d 
         ;;
     stop)
         echo "Stopping aesdsocket"
